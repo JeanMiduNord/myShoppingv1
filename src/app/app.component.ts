b69c2cec9c05cb47bd8023291067977ec15b9c1c
JeanMichel <jm.quetelart@free.fr>
2020-10-01 11:49:07 +0200

    chap 6 (pipe date async)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'myShopping toto';
   isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
 
   appareils = [
     {
